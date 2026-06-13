008d3d487dac3e550e4a2824d23870fbdf2bb540
frappe <frappe@DESKTOP-RIU4LV8.localdomain>
2026-06-13 11:02:03 +0500

    New Doctype

---
+++ surmawala_accounts/surmawala_accounts/doctype/admission/admission.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Ahsan Shah and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Admission(Document):
+	pass
